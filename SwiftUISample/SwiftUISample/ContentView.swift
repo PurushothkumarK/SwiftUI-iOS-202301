18f834e476b6e1194b561286c0603e63e612930a
Purushothkumar <purushothkumar@hearzap.com>
2023-08-01 16:20:00 +0530

    Button

--- SwiftUISample/SwiftUISample/ContentView.swift
+++ SwiftUISample/SwiftUISample/ContentView.swift
@@ -8,13 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showDetails = false
+    
     var body: some View {
-        VStack {
+        VStack(spacing:30) {
             Image(systemName: "globe")
                 .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+                .foregroundColor(.red)
+                .scaledToFill()
+            Divider().foregroundColor(.blue)
+            Text("Hello, world!").foregroundColor(.red)
+            Button("Button title") {
+                print("button tapped")
+            }
+            Button("Show details") {
+                showDetails.toggle()
+            }
+            Button("Delete", role: .destructive) {
+                print("Perform delete")
+            }
+            if showDetails {
+                Text("You should follow me on Twitter: @twostraws")
+                    .font(.largeTitle)
+            }
+            Button {
+                print("Button pressed")
+            } label: {
+                Text("Press Me")
+                    .padding(20)
+            }
         }
+        
         .padding()
     }
 }
